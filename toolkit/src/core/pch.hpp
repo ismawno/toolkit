ebf86ceb09ec2a8f48f51cf91ad0433d9c4ef56b
Ismael Bueno <ismaelwno@gmail.com>
2024-08-03 01:20:04 +0100

    Add pre compiled headers

---
+++ toolkit/src/core/pch.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+#include <string>
+#include <vector>
+#include <memory>
+#include <functional>
+#include <chrono>
+#include <algorithm>
+#include <iostream>
+#include <type_traits>
+#include <utility>
+#include <array>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <tuple>
+#include <source_location>
