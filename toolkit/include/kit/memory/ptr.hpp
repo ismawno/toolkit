f373063935675824b42c4bcef5a0ad5903a3836d
Ismael Bueno <ismaelwno@gmail.com>
2024-08-17 21:48:31 +0100

    Remove constraint in std namespace

--- toolkit/include/kit/memory/ptr.hpp
+++ toolkit/include/kit/memory/ptr.hpp
@@ -219,7 +219,7 @@ KIT_NAMESPACE_END
 
 namespace std
 {
-template <KIT_NAMESPACE_NAME::RCounted<KIT_NAMESPACE_NAME::RefCounted> T> struct hash<KIT_NAMESPACE_NAME::Ref<T>>
+template <typename T> struct hash<KIT_NAMESPACE_NAME::Ref<T>>
 {
     KIT_NAMESPACE_NAME::usz operator()(const KIT_NAMESPACE_NAME::Ref<T> &p_Ref) const KIT_NOEXCEPT
     {
