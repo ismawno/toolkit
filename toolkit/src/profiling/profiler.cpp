b54e91f2f04b3526b84f4ef0534ae1fa364d152c
Ismael Bueno <ismaelwno@gmail.com>
2024-08-18 15:14:50 +0100

    Profiler is no longer thread safe: there is no point and Begin/End calls must still be synchronized

--- toolkit/src/profiling/profiler.cpp
+++ toolkit/src/profiling/profiler.cpp
@@ -1,10 +1,7 @@
 #include "core/pch.hpp"
 #include "kit/profiling/profiler.hpp"
-#include <mutex>
 
 KIT_NAMESPACE_BEGIN
-
-static std::mutex s_Mutex;
 
 Profiler::Timer::Timer(const char *name) KIT_NOEXCEPT
 {
@@ -17,14 +14,12 @@ Profiler::Timer::~Timer() KIT_NOEXCEPT
 
 void Profiler::BeginMeasurement(const char *name) KIT_NOEXCEPT
 {
-    std::scoped_lock lock(s_Mutex);
     s_OngoingMeasurements.emplace_back(name, Clock{});
 }
 
 void Profiler::EndMeasurement() KIT_NOEXCEPT
 {
     Measurement measurement;
-    std::scoped_lock lock(s_Mutex);
 
     const auto [name, clock] = s_OngoingMeasurements.back();
     measurement.Name = name;
@@ -41,7 +36,6 @@ const Measurement &Profiler::GetLast() KIT_NOEXCEPT
 
 void Profiler::Clear() KIT_NOEXCEPT
 {
-    std::scoped_lock lock(s_Mutex);
     s_Measurements.clear();
 }
 
@@ -58,9 +52,6 @@ const DynamicArray<Measurement> &Profiler::Measurements() KIT_NOEXCEPT
 HashMap<const char *, AggregatedMeasurement> Profiler::AggregateMeasurements() KIT_NOEXCEPT
 {
     HashMap<const char *, AggregatedMeasurement> aggregated;
-
-    // Should actually be a shared lock, but this method is not supposed to be called often
-    std::scoped_lock lock(s_Mutex);
 
     for (const Measurement &measurement : s_Measurements)
     {
