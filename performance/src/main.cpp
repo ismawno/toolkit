9721f7d03c3ecc875b89b8b79116f16bdaac9716
Ismael Bueno <ismaelwno@gmail.com>
2024-08-21 01:53:10 +0100

    Add a bit of logging to performance run

--- performance/src/main.cpp
+++ performance/src/main.cpp
@@ -1,6 +1,7 @@
 #include "perf/memory.hpp"
 #include "perf/multiprocessing.hpp"
 #include "kit/memory/block_allocator.hpp"
+#include "kit/profiling/clock.hpp"
 
 using namespace KIT_NAMESPACE_NAME;
 
@@ -8,14 +9,28 @@ int main()
 {
     const Settings settings = ReadOrWriteSettingsFile();
 
-    RecordThreadPoolSum<SpinMutex>(settings.ThreadPoolSum, settings.MaxThreads);
+    Clock clock;
+    KIT_LOG_INFO("Running thread pool sum...");
+    RecordThreadPoolSum<std::mutex>(settings.ThreadPoolSum, settings.MaxThreads);
+
+    KIT_LOG_INFO("Running parallel sum...");
     RecordParallelSum(settings.ThreadPoolSum, settings.MaxThreads);
 
+    KIT_LOG_INFO("Running malloc/free ST...");
     RecordMallocFreeST(settings.Allocation);
+
+    KIT_LOG_INFO("Running block allocator ST...");
     RecordBlockAllocatorST(settings.Allocation);
 
+    KIT_LOG_INFO("Running malloc/free MT...");
     RecordMallocFreeMT(settings.Allocation, settings.MaxThreads);
+
+    KIT_LOG_INFO("Running block allocator MT...");
     RecordBlockAllocatorMT(settings.Allocation, settings.MaxThreads);
 
+    KIT_LOG_INFO("Running stack allocator...");
     RecordStackAllocator(settings.Allocation);
+
+    KIT_LOG_INFO("Done! ({.1f} seconds) Results have been written to 'performance/results'",
+                 clock.Elapsed().AsSeconds());
 }
