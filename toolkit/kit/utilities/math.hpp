68bf2e11f5cce6dd097506edc0f766ade9b2fe41
Ismael Bueno <ismaelwno@gmail.com>
2024-11-13 19:57:39 +0100

    Re order utility function so they are visible to each other

--- toolkit/kit/utilities/math.hpp
+++ toolkit/kit/utilities/math.hpp
@@ -4,14 +4,14 @@
 
 namespace KIT
 {
-template <std::floating_point Float> bool Approximately(const Float a, const Float b)
-{
-    return ApproachesZero(a - b);
-}
-
 template <std::floating_point Float> bool ApproachesZero(const Float x)
 {
     return std::abs(x) <= std::numeric_limits<Float>::epsilon();
+}
+
+template <std::floating_point Float> bool Approximately(const Float a, const Float b)
+{
+    return ApproachesZero(a - b);
 }
 
 template <typename Vec2> auto Cross2D(const Vec2 &a, const Vec2 &b)
