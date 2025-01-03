3bbd8a0e60d0fae000fe49392fe1372b4215e111
Ismael Bueno <ismaelwno@gmail.com>
2025-01-03 23:44:06 +0000

    Improve profiling API by avoiding weird macro design choices, for vulkan as well

--- toolkit/tkit/profiling/vulkan.hpp
+++ toolkit/tkit/profiling/vulkan.hpp
@@ -16,14 +16,15 @@ using VkProfilingContext = TracyVkCtx;
 
 #    define TKIT_PROFILE_DESTROY_VULKAN_CONTEXT(p_Context) TracyVkDestroy(p_Context)
 
-#    define TKIT_PROFILE_VULKAN_SCOPE(p_Name, p_Context, p_Cmdbuf) TracyVkZone(p_Context, p_Cmdbuf, p_Name)
-#    define TKIT_PROFILE_VULKAN_CSCOPE(p_Name, p_Color, p_Context, p_Cmdbuf)                                           \
-        TracyVkZoneC(p_Context, p_Cmdbuf, p_Name, p_Color)
+#    define TKIT_PROFILE_VULKAN_SCOPE_IF(p_Active, p_Name, p_Context, p_Cmdbuf)                                        \
+        TracyVkNamedZone(p_Context, __tkit_gpu_scope##__LINE__, p_Cmdbuf, p_Name, p_Active)
+#    define TKIT_PROFILE_VULKAN_CSCOPE_IF(p_Active, p_Name, p_Color, p_Context, p_Cmdbuf)                              \
+        TracyVkNamedZoneC(p_Context, __tkit_gpu_scope##__LINE__, p_Cmdbuf, p_Name, p_Color, p_Active)
 
-#    define TKIT_PROFILE_VULKAN_NAMED_SCOPE(p_ScopeName, p_Name, p_Context, p_Cmdbuf, p_Active)                        \
-        TracyVkNamedZone(p_Context, p_ScopeName, p_Cmdbuf, p_Name, p_Active)
-#    define TKIT_PROFILE_VULKAN_NAMED_CSCOPE(p_ScopeName, p_Name, p_Color, p_Context, p_Cmdbuf, p_Active)              \
-        TracyVkNamedZoneC(p_Context, p_ScopeName, p_Cmdbuf, p_Name, p_Color, p_Active)
+#    define TKIT_PROFILE_VULKAN_SCOPE(p_Name, p_Context, p_Cmdbuf)                                                     \
+        TracyVkNamedZone(p_Context, __tkit_gpu_scope##__LINE__, p_Cmdbuf, p_Name, true)
+#    define TKIT_PROFILE_VULKAN_CSCOPE(p_Name, p_Color, p_Context, p_Cmdbuf)                                           \
+        TracyVkNamedZoneC(p_Context, __tkit_gpu_scope##__LINE__, p_Cmdbuf, p_Name, p_Color, true)
 
 #    define TKIT_PROFILE_VULKAN_COLLECT(p_Context, p_Cmdbuf) TracyVkCollect(p_Context, p_Cmdbuf)
 
@@ -32,13 +33,15 @@ using VkProfilingContext = TracyVkCtx;
 #    define TKIT_PROFILE_CREATE_VULKAN_CONTEXT(p_Instance, p_Physdev, p_Device, p_Queue, p_Cmdbuf, p_InstanceProcAddr, \
                                                p_DeviceProcAddr)                                                       \
         nullptr
+#    define TKIT_PROFILE_CREATE_VULKAN_CALIBRATED_CONTEXT(p_Physdev, p_Device, p_Queue, p_Cmdbuf, p_Gpdctd, p_Gct)
+
 #    define TKIT_PROFILE_DESTROY_VULKAN_CONTEXT(p_Context)
+
+#    define TKIT_PROFILE_VULKAN_SCOPE_IF(p_Active, p_Name, p_Context, p_Cmdbuf)
+#    define TKIT_PROFILE_VULKAN_CSCOPE_IF(p_Active, p_Name, p_Color, p_Context, p_Cmdbuf)
 
 #    define TKIT_PROFILE_VULKAN_SCOPE(p_Name, p_Context, p_Cmdbuf)
 #    define TKIT_PROFILE_VULKAN_CSCOPE(p_Name, p_Color, p_Context, p_Cmdbuf)
-
-#    define TKIT_PROFILE_VULKAN_NAMED_SCOPE(p_ScopeName, p_Name, p_Context, p_Cmdbuf, p_Active)
-#    define TKIT_PROFILE_VULKAN_NAMED_CSCOPE(p_ScopeName, p_Name, p_Color, p_Context, p_Cmdbuf, p_Active)
 
 #    define TKIT_PROFILE_VULKAN_COLLECT(p_Context, p_Cmdbuf)
 
