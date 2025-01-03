36f4167c91f6416280edde469b2adc4b68d4dc95
Ismael Bueno <ismaelwno@gmail.com>
2025-01-03 23:36:14 +0000

    Improve profiling API by avoiding weird macro design choices

--- toolkit/tkit/profiling/macros.hpp
+++ toolkit/tkit/profiling/macros.hpp
@@ -7,27 +7,25 @@
 
 #    include "tracy/Tracy.hpp"
 
-#    define TKIT_PROFILE_MARK_FRAME FrameMark
+#    define TKIT_PROFILE_MARK_FRAME() FrameMark
 #    define TKIT_PROFILE_MARK_FRAME_START(p_Name) FrameMarkStart(p_Name)
 #    define TKIT_PROFILE_MARK_FRAME_END(p_Name) FrameMarkEnd(p_Name)
 #    define TKIT_PROFILE_NMARK_FRAME(p_Name) FrameMarkNamed(p_Name)
 
-#    define TKIT_PROFILE_SCOPE ZoneScoped
-#    define TKIT_PROFILE_NSCOPE(p_Name) ZoneScopedN(p_Name)
-#    define TKIT_PROFILE_CSCOPE(p_Color) ZoneScopedC(p_Color)
-#    define TKIT_PROFILE_NCSCOPE(p_Name, p_Color) ZoneScopedNC(p_Name, p_Color)
-#    define TKIT_PROFILE_SCOPE_TEXT(p_Text, p_Size) ZoneText(p_Text, p_Size)
-#    define TKIT_PROFILE_SCOPE_NAME(p_Name, p_Size) ZoneName(p_Name, p_Size)
-#    define TKIT_PROFILE_SCOPE_COLOR(p_Color) ZoneColor(p_Color)
+#    define TKIT_PROFILE_SCOPE_IF(p_Enabled) ZoneNamed(__tkit_perf_scope##__LINE__, p_Enabled)
+#    define TKIT_PROFILE_NSCOPE_IF(p_Enabled, p_Name) ZoneNamedN(__tkit_perf_scope##__LINE__, p_Name, p_Enabled)
+#    define TKIT_PROFILE_CSCOPE_IF(p_Enabled, p_Color) ZoneNamedC(__tkit_perf_scope##__LINE__, p_Color, p_Enabled)
+#    define TKIT_PROFILE_NCSCOPE_IF(p_Enabled, p_Name, p_Color)                                                        \
+        ZoneNamedNC(__tkit_perf_scope##__LINE__, p_Name, p_Color, p_Enabled)
 
-#    define TKIT_PROFILE_NAMED_SCOPE(p_ScopeName, p_Enabled) ZoneNamed(p_ScopeName, p_Enabled)
-#    define TKIT_PROFILE_NAMED_NSCOPE(p_ScopeName, p_Name, p_Enabled) ZoneNamedN(p_ScopeName, p_Name, p_Enabled)
-#    define TKIT_PROFILE_NAMED_CSCOPE(p_ScopeName, p_Color, p_Enabled) ZoneNamedC(p_ScopeName, p_Color, p_Enabled)
-#    define TKIT_PROFILE_NAMED_NCSCOPE(p_ScopeName, p_Name, p_Color, p_Enabled)                                        \
-        ZoneNamedNC(p_ScopeName, p_Name, p_Color, p_Enabled)
-#    define TKIT_PROFILE_NAMED_SCOPE_TEXT(p_ScopeName, p_Text, p_Size) ZoneTextV(p_ScopeName, p_Text, p_Size)
-#    define TKIT_PROFILE_NAMED_SCOPE_NAME(p_ScopeName, p_Name, p_Size) ZoneNameV(p_ScopeName, p_Name, p_Size)
-#    define TKIT_PROFILE_NAMED_SCOPE_COLOR(p_ScopeName, p_Color) ZoneColorV(p_ScopeName, p_Color)
+#    define TKIT_PROFILE_SCOPE() ZoneNamed(__tkit_perf_scope##__LINE__, true)
+#    define TKIT_PROFILE_NSCOPE(p_Name) ZoneNamedN(__tkit_perf_scope##__LINE__, p_Name, true)
+#    define TKIT_PROFILE_CSCOPE(p_Color) ZoneNamedC(__tkit_perf_scope##__LINE__, p_Color, true)
+#    define TKIT_PROFILE_NCSCOPE(p_Name) ZoneNamedNC(__tkit_perf_scope##__LINE__, p_Name, true)
+
+#    define TKIT_PROFILE_SCOPE_TEXT(p_Text, p_Size) ZoneTextV(__tkit_perf_scope##__LINE__, p_Text, p_Size)
+#    define TKIT_PROFILE_SCOPE_NAME(p_Name, p_Size) ZoneNameV(__tkit_perf_scope##__LINE__, p_Name, p_Size)
+#    define TKIT_PROFILE_SCOPE_COLOR(p_Color) ZoneColorV(__tkit_perf_scope##__LINE__, p_Color)
 
 #    define TKIT_PROFILE_MESSAGE(p_Message) TracyMessageL(p_Message)
 #    define TKIT_PROFILE_NMESSAGE(p_Message, p_Size) TracyMessage(p_Message, p_Size)
@@ -44,26 +42,24 @@
 
 #else
 
-#    define TKIT_PROFILE_MARK_FRAME
+#    define TKIT_PROFILE_MARK_FRAME()
 #    define TKIT_PROFILE_MARK_FRAME_START(p_Name)
 #    define TKIT_PROFILE_MARK_FRAME_END(p_Name)
 #    define TKIT_PROFILE_NMARK_FRAME(p_Name)
 
-#    define TKIT_PROFILE_SCOPE
+#    define TKIT_PROFILE_SCOPE_IF(p_Enabled)
+#    define TKIT_PROFILE_NSCOPE_IF(p_Enabled, p_Name)
+#    define TKIT_PROFILE_CSCOPE_IF(p_Enabled, p_Color)
+#    define TKIT_PROFILE_NCSCOPE_IF(p_Enabled, p_Name, p_Color)
+
+#    define TKIT_PROFILE_SCOPE()
 #    define TKIT_PROFILE_NSCOPE(p_Name)
 #    define TKIT_PROFILE_CSCOPE(p_Color)
 #    define TKIT_PROFILE_NCSCOPE(p_Name)
+
 #    define TKIT_PROFILE_SCOPE_TEXT(p_Text, p_Size)
 #    define TKIT_PROFILE_SCOPE_NAME(p_Name, p_Size)
 #    define TKIT_PROFILE_SCOPE_COLOR(p_Color)
-
-#    define TKIT_PROFILE_NAMED_SCOPE(p_ScopeName, p_Enabled)
-#    define TKIT_PROFILE_NAMED_NSCOPE(p_ScopeName, p_Name, p_Enabled)
-#    define TKIT_PROFILE_NAMED_CSCOPE(p_ScopeName, p_Color, p_Enabled)
-#    define TKIT_PROFILE_NAMED_NCSCOPE(p_ScopeName, p_Name, p_Color, p_Enabled)
-#    define TKIT_PROFILE_NAMED_SCOPE_TEXT(p_ScopeName, p_Text, p_Size)
-#    define TKIT_PROFILE_NAMED_SCOPE_NAME(p_ScopeName, p_Name, p_Size)
-#    define TKIT_PROFILE_NAMED_SCOPE_COLOR(p_ScopeName, p_Color)
 
 #    define TKIT_PROFILE_MESSAGE(p_Message)
 #    define TKIT_PROFILE_NMESSAGE(p_Message, p_Size)
