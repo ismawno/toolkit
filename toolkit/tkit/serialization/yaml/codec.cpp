136f2dbf09910f5c130271513d0edcf1a7617194
Ismael Bueno <ismaelwno@gmail.com>
2025-02-22 19:51:21 +0100

    Add check if path does not exist

--- toolkit/tkit/serialization/yaml/codec.cpp
+++ toolkit/tkit/serialization/yaml/codec.cpp
@@ -1,5 +1,6 @@
 #include "tkit/core/pch.hpp"
 #include "tkit/serialization/yaml/codec.hpp"
+#include <filesystem>
 
 namespace TKit::Yaml
 {
@@ -9,6 +10,7 @@ Node LoadFromString(const std::string_view p_String) noexcept
 }
 Node LoadFromFile(const std::string_view p_Path) noexcept
 {
+    TKIT_ASSERT(std::filesystem::exists(p_Path.data()), "File does not exist: {}", p_Path);
     return YAML::LoadFile(p_Path.data());
 }
 } // namespace TKit::Yaml
