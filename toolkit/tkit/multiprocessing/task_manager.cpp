88e6087b29a45bbac51ed2466310e0105f4f697f
Ismael Bueno <ismaelwno@gmail.com>
2025-05-28 18:16:33 +0200

    Prevent thread counts not being greater than 0

--- toolkit/tkit/multiprocessing/task_manager.cpp
+++ toolkit/tkit/multiprocessing/task_manager.cpp
@@ -1,10 +1,12 @@
 #include "tkit/core/pch.hpp"
+#include "tkit/utils/logging.hpp"
 #include "tkit/multiprocessing/task_manager.hpp"
 
 namespace TKit
 {
 ITaskManager::ITaskManager(const usize p_ThreadCount) noexcept : m_ThreadCount(p_ThreadCount)
 {
+    TKIT_ASSERT(p_ThreadCount != 0, "The thread count must be greater than 0.");
 }
 
 usize ITaskManager::GetThreadCount() const noexcept
