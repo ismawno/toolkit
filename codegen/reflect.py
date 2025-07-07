6322fb1e5c4ab1d358e07a46903180ffc2acaebb
Ismael Bueno <ismaelwno@gmail.com>
2025-07-07 16:36:13 +0100

    Update reflection script

--- codegen/reflect.py
+++ codegen/reflect.py
@@ -89,6 +89,7 @@ hpp.include("tkit/container/array.hpp", quotes=True)
 hpp.include("tkit/reflection/reflect.hpp", quotes=True)
 hpp.include("tkit/utils/concepts.hpp", quotes=True)
 hpp.include("tuple")
+hpp.include("string_view")
 
 with hpp.scope("namespace TKit", indent=0):
     used_namespaces = ["TKit"]
@@ -116,8 +117,11 @@ with hpp.scope("namespace TKit", indent=0):
                 hpp("Protected = 1,")
                 hpp("Public = 2")
 
+            hpp(f"static constexpr bool HasMemberFields = {'true' if clsinfo.memfields.all else 'false'};")
+            hpp(f"static constexpr bool HasStaticFields = {'true' if clsinfo.statfields.all else 'false'};")
+
             def generate_reflect_body(fcollection: FieldCollection, /, *, is_static: bool) -> None:
-                static = "Static" if is_static else ""
+                modifier = "Static" if is_static else "Member"
 
                 hpp("public:")
                 dtype = "u8" if len(clsinfo.memfields.per_group) < 256 else "u16"
@@ -128,23 +132,29 @@ with hpp.scope("namespace TKit", indent=0):
                         )
                         hpp("Its values may be used as compile-time filters in certain functions.")
 
-                    with hpp.scope(f"enum class {static}Group : {dtype}", closer="};"):
+                    with hpp.scope(f"enum class {modifier}Group : {dtype}", closer="};"):
                         for i, group in enumerate(fcollection.per_group):
                             hpp(f"{group} = {i},")
 
                 with hpp.doc():
-                    hpp.brief(f"Encapsulates specific information about a `{clsinfo.name}` field.")
+                    hpp.brief(f"Encapsulates specific information about a `{clsinfo.name}` {modifier.lower()} field.")
                     hpp("You may access an arbitrary field's value at run-time with the help of this struct.")
+                    hpp(
+                        "A field may be null, which means it does not represent any real field. You may check for its validity using the bool operator. This is only needed when querying a specific field by name."
+                    )
 
-                with hpp.scope(f"template <typename Ref_Type> struct {static}Field", closer="};"):
+                with hpp.scope(f"template <typename Ref_Type> struct {modifier}Field", closer="};"):
                     hpp("using Type = Ref_Type;")
-                    hpp("const char *Name;")
-                    hpp("const char *TypeString;")
+                    hpp("const char *Name = nullptr;")
+                    hpp("const char *TypeString = nullptr;")
                     if is_static:
-                        hpp("Ref_Type *Pointer;")
+                        hpp("Ref_Type *Pointer = nullptr;")
                     else:
-                        hpp(f"Ref_Type {clsinfo.name}::* Pointer;")
-                    hpp("FieldVisibility Visibility;")
+                        hpp(f"Ref_Type {clsinfo.name}::* Pointer = nullptr;")
+                    hpp("FieldVisibility Visibility{};")
+
+                    with hpp.scope("operator bool() const noexcept"):
+                        hpp("return Name && TypeString && Pointer;")
 
                     if not is_static:
 
@@ -175,15 +185,66 @@ with hpp.scope("namespace TKit", indent=0):
                         ):
                             hpp("p_Instance.*Pointer = std::forward<U>(p_Value);")
 
-                def create_cpp_fields_sequence(fields: list[Field], /, *, group: str | None = None) -> list[str]:
+                def create_if_constexpr_per_type(
+                    fn: Callable[..., None],
+                    null: str,
+                    /,
+                    *,
+                    group: str | None = None,
+                    as_sequence: bool = True,
+                    delimiters: bool = False,
+                ) -> None:
+                    cnd = "if"
+                    for vtype, fields in fcollection.per_type.items():
+                        if as_sequence:
+                            fields = create_cpp_fields_sequence(fields, group=group)
+                        if not fields:
+                            continue
+                        with hpp.scope(
+                            f"{cnd} constexpr (std::is_same_v<Ref_Type, {vtype}>)",
+                            delimiters=delimiters,
+                        ):
+                            fn(fields, vtype)
+                        cnd = "else if"
+                    if cnd == "else if":
+                        with hpp.scope("else", delimiters=False):
+                            hpp(f"return {null};")
+                    else:
+                        hpp(f"return {null};")
+
+                def convert_field_to_cpp(field: Field, /) -> str:
                     def replacer(vtype: str, /) -> str:
                         return vtype.replace('"', r"\"")
 
-                    return [
-                        f'{static}Field<{field.vtype}>{{"{field.name}", "{replacer(field.vtype)}", &{clsinfo.name}::{field.name}, FieldVisibility::{field.visibility.capitalize()}}}'
-                        for field in fields
-                        if group is None or group in field.groups
-                    ]
+                    return f'{modifier}Field<{field.vtype}>{{"{field.name}", "{replacer(field.vtype)}", &{clsinfo.name}::{field.name}, FieldVisibility::{field.visibility.capitalize()}}}'
+
+                def create_cpp_fields_sequence(fields: list[Field], /, *, group: str | None = None) -> list[str]:
+                    return [convert_field_to_cpp(field) for field in fields if group is None or group in field.groups]
+
+                with hpp.doc():
+                    hpp.brief("Get a specific field by name.")
+                    hpp("If the field does not exist, an empty field will be returned.")
+                    hpp.tparam("Ref_Type", "The type of the field. A global, type agnostic query is not supported.")
+                    hpp.param("p_Field", "The name of the field.")
+
+                with hpp.scope(
+                    f"template <typename Ref_Type> static constexpr auto Get{modifier}Field(const std::string_view p_Field) noexcept"
+                ):
+
+                    def generator(fields: list[Field], vtype: str, /) -> None:
+                        for field in fields:
+                            with hpp.scope(f'if (p_Field == "{field.name}")', delimiters=False):
+                                hpp(f"return {convert_field_to_cpp(field)};")
+                        hpp(f"return Field<{vtype}>{{}};")
+
+                    create_if_constexpr_per_type(generator, "Field<u8>{}", as_sequence=False, delimiters=True)
+
+                with hpp.scope(f"static constexpr bool Has{modifier}Field(const std::string p_Field) noexcept"):
+                    for vtype in fcollection.per_type:
+                        with hpp.scope(f"if (Get{modifier}Field<{vtype}>(p_Field))", delimiters=False):
+                            hpp("return true;")
+
+                    hpp("return false;")
 
                 def create_tuple_sequence(fields: list[str], /, **_) -> str:
                     return f"std::make_tuple({', '.join(fields)})"
@@ -191,13 +252,13 @@ with hpp.scope("namespace TKit", indent=0):
                 def create_array_sequence(fields: list[str], /, *, vtype: str | None = None) -> str:
                     if vtype is None:
                         vtype = "Ref_Type"
-                    return f"Array<{static}Field<{vtype}>, {len(fields)}>{{{', '.join(fields)}}}"
+                    return f"Array<{modifier}Field<{vtype}>, {len(fields)}>{{{', '.join(fields)}}}"
 
                 def create_get_fields_method(fields: list[Field], /, *, group: str = "") -> None:
                     fields_cpp = create_cpp_fields_sequence(fields)
 
                     with hpp.doc():
-                        msg = "Get all " + ("static " if static else "") + "fields"
+                        msg = f"Get all {modifier.lower()} fields"
                         if group:
                             msg += f" for the `{group}` group."
                         else:
@@ -212,7 +273,7 @@ with hpp.scope("namespace TKit", indent=0):
                         )
 
                     with hpp.scope(
-                        f"template <typename... Ref_Types> static constexpr auto Get{static}{group}Fields() noexcept"
+                        f"template <typename... Ref_Types> static constexpr auto Get{modifier}{group}Fields() noexcept"
                     ):
                         with hpp.scope("if constexpr (sizeof...(Ref_Types) == 0)", delimiters=False):
                             hpp(f"return {create_tuple_sequence(fields_cpp)};")
@@ -220,13 +281,13 @@ with hpp.scope("namespace TKit", indent=0):
                             "else if constexpr (sizeof...(Ref_Types) == 1)",
                             delimiters=False,
                         ):
-                            hpp(f"return get{static}{group}Array<Ref_Types...>();")
+                            hpp(f"return get{modifier}{group}Array<Ref_Types...>();")
                         with hpp.scope("else", delimiters=False):
-                            hpp(f"return std::tuple_cat(get{static}{group}Tuple<Ref_Types>()...);")
+                            hpp(f"return std::tuple_cat(get{modifier}{group}Tuple<Ref_Types>()...);")
 
                 def create_for_each_method(*, group: str = "") -> None:
                     with hpp.doc():
-                        msg = "Iterate over all " + ("static " if static else "") + "fields"
+                        msg = f"Iterate over all {modifier.lower()} fields"
                         if group:
                             msg += f" for the `{group}` group."
                         else:
@@ -242,14 +303,13 @@ with hpp.scope("namespace TKit", indent=0):
                         )
 
                     with hpp.scope(
-                        f"template <typename... Ref_Types, typename Ref_Fun> static constexpr void ForEach{static}{group}Field(Ref_Fun &&p_Fun) noexcept"
+                        f"template <typename... Ref_Types, typename Ref_Fun> static constexpr void ForEach{modifier}{group}Field(Ref_Fun &&p_Fun) noexcept"
                     ):
-                        hpp(f"const auto fields = Get{static}{group}Fields<Ref_Types...>();")
-                        hpp(f"ForEach{static}Field(fields, std::forward<Ref_Fun>(p_Fun));")
+                        hpp(f"const auto fields = Get{modifier}{group}Fields<Ref_Types...>();")
+                        hpp(f"ForEach{modifier}Field(fields, std::forward<Ref_Fun>(p_Fun));")
 
                 with hpp.doc():
-                    msg = "Iterate over all " + ("static " if static else "") + "fields."
-                    hpp.brief(msg)
+                    hpp.brief(f"Iterate over all {modifier.lower()} fields.")
                     hpp.tparam(
                         "Ref_Types",
                         "You may optionally provide type filters so that only fields with such types are retrieved.",
@@ -264,7 +324,7 @@ with hpp.scope("namespace TKit", indent=0):
                     )
 
                 with hpp.scope(
-                    f"template <typename Ref_Type, typename Ref_Fun> static constexpr void For{static}EachField(const Ref_Type &p_Fields, Ref_Fun &&p_Fun) noexcept"
+                    f"template <typename Ref_Type, typename Ref_Fun> static constexpr void For{modifier}EachField(const Ref_Type &p_Fields, Ref_Fun &&p_Fun) noexcept"
                 ):
                     with hpp.scope("if constexpr (Iterable<Ref_Type>)", delimiters=False):
                         with hpp.scope(
@@ -282,8 +342,7 @@ with hpp.scope("namespace TKit", indent=0):
 
                 if fcollection.per_group:
                     with hpp.doc():
-                        msg = "Get all " + ("static " if static else "") + "fields filtered by a specific group enum."
-                        hpp.brief(msg)
+                        hpp.brief(f"Get all {modifier.lower()} fields.")
                         hpp.tparam("Ref_Group", "A compile-time group value to filter the retrieved fields.")
                         hpp.tparam(
                             "Ref_Types",
@@ -293,32 +352,27 @@ with hpp.scope("namespace TKit", indent=0):
                             "If the retrieved fields share all the same type, the return value will be simplified to a `TKit::Array<Field>`. If that is not the case however, the fields will be stored in a `std::tuple`."
                         )
                     with hpp.scope(
-                        f"template <Group Ref_Group, typename... Ref_Types> static constexpr auto Get{static}FieldsByGroup() noexcept"
+                        f"template <Group Ref_Group, typename... Ref_Types> static constexpr auto Get{modifier}FieldsByGroup() noexcept"
                     ):
                         cnd = "if"
                         for group in fcollection.per_group:
                             with hpp.scope(
-                                f"{cnd} constexpr (Ref_Group == {static}Group::{group})",
+                                f"{cnd} constexpr (Ref_Group == {modifier}Group::{group})",
                                 delimiters=False,
                             ):
-                                hpp(f"return Get{static}{group}Fields<Ref_Types...>();")
+                                hpp(f"return Get{modifier}{group}Fields<Ref_Types...>();")
                             cnd = "else if"
 
                         with hpp.scope(
                             "else if constexpr (sizeof...(Ref_Types) == 1)",
                             delimiters=False,
                         ):
-                            hpp(f"return Array<{static}Field<Ref_Types...>, 0>{{}};")
+                            hpp(f"return Array<{modifier}Field<Ref_Types...>, 0>{{}};")
                         with hpp.scope("else", delimiters=False):
                             hpp("return std::tuple{};")
 
                     with hpp.doc():
-                        msg = (
-                            "Iterate over all "
-                            + ("static " if static else "")
-                            + "fields filtered by a specific group enum."
-                        )
-                        hpp.brief(msg)
+                        hpp.brief(f"Iterate over all {modifier.lower()} fields.")
                         hpp.tparam("Ref_Group", "A compile-time group value to filter the retrieved fields.")
                         hpp.tparam(
                             "Ref_Types",
@@ -329,52 +383,36 @@ with hpp.scope("namespace TKit", indent=0):
                             "A callable object that must accept any field type. The most straightforward way of doing so is by declaring the macro as `[](const auto &p_Field){};`.",
                         )
                     with hpp.scope(
-                        f"template <Group Ref_Group, typename... Ref_Types, typename Ref_Fun> static constexpr void ForEach{static}FieldByGroup(Ref_Fun &&p_Fun) noexcept"
+                        f"template <Group Ref_Group, typename... Ref_Types, typename Ref_Fun> static constexpr void ForEach{modifier}FieldByGroup(Ref_Fun &&p_Fun) noexcept"
                     ):
-                        hpp(f"const auto fields = Get{static}FieldsByGroup<Ref_Group, Ref_Types...>();")
-                        hpp(f"ForEach{static}Field(fields, std::forward<Ref_Fun>(p_Fun));")
+                        hpp(f"const auto fields = Get{modifier}FieldsByGroup<Ref_Group, Ref_Types...>();")
+                        hpp(f"ForEach{modifier}Field(fields, std::forward<Ref_Fun>(p_Fun));")
 
                 for group, fields in fcollection.per_group.items():
                     create_get_fields_method(fields, group=group.name)
                     create_for_each_method(group=group.name)
 
-                def create_if_constexpr_per_type(
-                    seq_creator: Callable[..., str],
-                    null: str,
-                    /,
-                    *,
-                    group: str | None = None,
-                ) -> None:
-                    cnd = "if"
-                    for vtype, fields in fcollection.per_type.items():
-                        fields_cpp = create_cpp_fields_sequence(fields, group=group)
-                        if not fields_cpp:
-                            continue
-                        with hpp.scope(
-                            f"{cnd} constexpr (std::is_same_v<Ref_Type, {vtype}>)",
-                            delimiters=False,
-                        ):
-                            hpp(f"return {seq_creator(fields_cpp, vtype=vtype)};")
-                        cnd = "else if"
-                    if cnd == "else if":
-                        with hpp.scope("else", delimiters=False):
-                            hpp(f"return {null};")
-                    else:
-                        hpp(f"return {null};")
-
                 def create_get_tuple_method(*, group: str | None = None) -> None:
                     with hpp.scope(
-                        f"template <typename Ref_Type> static constexpr auto get{static}{group if group is not None else ''}Tuple() noexcept"
+                        f"template <typename Ref_Type> static constexpr auto get{modifier}{group if group is not None else ''}Tuple() noexcept"
                     ):
-                        create_if_constexpr_per_type(create_tuple_sequence, "std::tuple{}", group=group)
+
+                        def generator(fields_cpp: list[str], vtype: str, /) -> None:
+                            hpp(f"return {create_tuple_sequence(fields_cpp, vtype=vtype)};")
+
+                        create_if_constexpr_per_type(generator, "std::tuple{}", group=group)
 
                 def create_get_array_method(*, group: str | None = None) -> None:
                     with hpp.scope(
-                        f"template <typename Ref_Type> static constexpr auto get{static}{group if group is not None else ''}Array() noexcept"
+                        f"template <typename Ref_Type> static constexpr auto get{modifier}{group if group is not None else ''}Array() noexcept"
                     ):
+
+                        def generator(fields_cpp: list[str], vtype: str, /) -> None:
+                            hpp(f"return {create_array_sequence(fields_cpp, vtype=vtype)};")
+
                         create_if_constexpr_per_type(
-                            create_array_sequence,
-                            f"Array<{static}Field<Ref_Type>, 0>{{}}",
+                            generator,
+                            f"Array<{modifier}Field<Ref_Type>, 0>{{}}",
                             group=group,
                         )
 
