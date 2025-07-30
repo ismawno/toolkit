bdc2aa7ffe8a5d828b1d919b3712f6795e5bedf0
Ismael Bueno <ismaelwno@gmail.com>
2025-07-30 23:59:53 +0100

    Fix profiling macro parameters

--- toolkit/tkit/profiling/macros.hpp
+++ toolkit/tkit/profiling/macros.hpp
@@ -26,7 +26,7 @@ using ProfilingPlotFormat = tracy::PlotFormatType;
 #    define TKIT_PROFILE_SCOPE() ZoneNamed(__tkit_perf_scope##__LINE__, true)
 #    define TKIT_PROFILE_NSCOPE(p_Name) ZoneNamedN(__tkit_perf_scope##__LINE__, p_Name, true)
 #    define TKIT_PROFILE_CSCOPE(p_Color) ZoneNamedC(__tkit_perf_scope##__LINE__, p_Color, true)
-#    define TKIT_PROFILE_NCSCOPE(p_Name) ZoneNamedNC(__tkit_perf_scope##__LINE__, p_Name, true)
+#    define TKIT_PROFILE_NCSCOPE(p_Name, p_Color) ZoneNamedNC(__tkit_perf_scope##__LINE__, p_Name, p_Color, true)
 
 #    define TKIT_PROFILE_SCOPE_TEXT(p_Text, p_Size) ZoneTextV(__tkit_perf_scope##__LINE__, p_Text, p_Size)
 #    define TKIT_PROFILE_SCOPE_NAME(p_Name, p_Size) ZoneNameV(__tkit_perf_scope##__LINE__, p_Name, p_Size)
@@ -64,7 +64,7 @@ using ProfilingPlotFormat = tracy::PlotFormatType;
 #    define TKIT_PROFILE_SCOPE()
 #    define TKIT_PROFILE_NSCOPE(p_Name)
 #    define TKIT_PROFILE_CSCOPE(p_Color)
-#    define TKIT_PROFILE_NCSCOPE(p_Name)
+#    define TKIT_PROFILE_NCSCOPE(p_Name, p_Color)
 
 #    define TKIT_PROFILE_SCOPE_TEXT(p_Text, p_Size)
 #    define TKIT_PROFILE_SCOPE_NAME(p_Name, p_Size)
