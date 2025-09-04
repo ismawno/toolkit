5ec414eb00ee54f2832df9e4d2141a1f3ade0f52
Ismael Bueno <ismaelwno@gmail.com>
2025-09-04 16:40:25 +0100

    Create a simple defer mechanism

---
+++ toolkit/tkit/utils/defer.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "tkit/utils/non_copyable.hpp"
+
+#define TKIT_DEFER_CAPTURE(p_Deletor, ...) const TKit::Defer __tkit_defer{[__VA_ARGS__] { p_Deletor; }};
+#define TKIT_DEFER(p_Deletor) const TKit::Defer __tkit_defer{[&] { p_Deletor; }};
+
+namespace TKit
+{
+template <typename F> struct Defer
+{
+    TKIT_NON_COPYABLE(Defer)
+    Defer(F &&p_Func) : Func(std::move(p_Func))
+    {
+    }
+
+    ~Defer()
+    {
+        Func();
+    }
+
+    F Func;
+};
+} // namespace TKit
