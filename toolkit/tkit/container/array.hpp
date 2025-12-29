58ac00c78c70a7bb0af3fa8825cdbdc9f9801172
Ismael Bueno <ismaelwno@gmail.com>
2025-12-29 14:44:48 +0100

    chore: Add aliases

--- toolkit/tkit/container/array.hpp
+++ toolkit/tkit/container/array.hpp
@@ -26,4 +26,17 @@ template <typename T, usize Capacity, typename Traits = Container::ArrayTraits<T
 using Array = DynamicArray<T, Traits>;
 #    define TKIT_ARRAY_RESERVE(p_Arr, p_Size) p_Arr.Reserve(p_Size)
 #endif
+
+template <typename T> using Array4 = Array<T, 4>;
+template <typename T> using Array8 = Array<T, 8>;
+template <typename T> using Array16 = Array<T, 16>;
+template <typename T> using Array32 = Array<T, 32>;
+template <typename T> using Array64 = Array<T, 64>;
+template <typename T> using Array128 = Array<T, 128>;
+template <typename T> using Array196 = Array<T, 196>;
+template <typename T> using Array256 = Array<T, 256>;
+template <typename T> using Array384 = Array<T, 384>;
+template <typename T> using Array512 = Array<T, 512>;
+template <typename T> using Array768 = Array<T, 768>;
+template <typename T> using Array1024 = Array<T, 1024>;
 } // namespace TKit
