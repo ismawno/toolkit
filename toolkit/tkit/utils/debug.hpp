8a458c4b6e8c778ee41e4170d02878f97faeeafe
Ismael Bueno <ismaelwno@gmail.com>
2026-01-05 12:15:10 +0100

    fix: Add preprocessor option for msvc

--- toolkit/tkit/utils/debug.hpp
+++ toolkit/tkit/utils/debug.hpp
@@ -32,7 +32,7 @@ void LogAndBreak(const char *p_Level, const char *p_Color, const char *p_File, c
 }
 inline void LogAndBreak(const char *p_Level, const char *p_Color, const char *p_File, const i32 p_Line)
 {
-    Log("Assertion failed!", p_Level, p_Color, p_File, p_Line);
+    Log("[TOOLKIT] Assertion failed!", p_Level, p_Color, p_File, p_Line);
     TKIT_DEBUG_BREAK();
 }
 #endif
