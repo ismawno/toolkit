bc9c6dbab0cea8b18367ab996232adcd9ac17cec
Ismael Bueno <ismaelwno@gmail.com>
2026-08-30 14:34:20 +0200

    Revert "feat: Add span conversion for hive"
    This reverts commit ecfab2085c857b3f976ceab44f44630a709d2327.

--- toolkit/tkit/container/span.hpp
+++ toolkit/tkit/container/span.hpp
@@ -2,7 +2,6 @@
 
 #include "tkit/container/fixed_array.hpp"
 #include "tkit/container/array.hpp"
-#include "tkit/container/hive.hpp"
 #include "tkit/math/tensor.hpp"
 #include <span>
 #include <vector>
@@ -61,15 +60,6 @@ template <typename T> class Span
     }
     template <typename AllocState>
     constexpr Span(Array<ElementType, AllocState> &array) : m_Data(array.GetData()), m_Size(array.GetSize())
-    {
-    }
-
-    template <typename AllocState>
-    constexpr Span(const Hive<ElementType, AllocState> &hive) : m_Data(hive.GetData(), hive.GetSize())
-    {
-    }
-    template <typename AllocState>
-    constexpr Span(Hive<ElementType, AllocState> &hive) : m_Data(hive.GetData(), hive.GetSize())
     {
     }
 
